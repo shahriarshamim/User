e070e22fe5c077c6cac13cadfc00c708facbb28e
Shahriar Shamim <shamimshahriar786@gmail.com>
2025-03-27 14:37:28 +0600

    commit
    Update Code

--- User/obj/Debug/net8.0/User.AssemblyInfo.cs
+++ User/obj/Debug/net8.0/User.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("User")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ec899f310b5c88f7973200697c6fb1294d766d")]
 [assembly: System.Reflection.AssemblyProductAttribute("User")]
 [assembly: System.Reflection.AssemblyTitleAttribute("User")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
