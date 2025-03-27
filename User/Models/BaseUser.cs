b0ec899f310b5c88f7973200697c6fb1294d766d
Shahriar Shamim <shamimshahriar786@gmail.com>
2025-03-27 14:33:01 +0600

    Initial commit

---
+++ User/Models/BaseUser.cs
@@ -0,0 +1,17 @@
+﻿namespace User.Models
+{
+    [Serializable]
+    public class BaseUser
+    {
+        //public int id { get; set; }
+        //public string? firstName { get; set; }
+        //public string? lastName { get; set; }
+        //public string? maidenName { get; set; }
+        //public int age { get; set; }
+        //public string? gender { get; set; }
+        //public string? email { get; set; }
+        //public string? phone { get; set; }
+        //public string? username { get; set; }
+        //public string? images { get; set; }
+    }
+}
